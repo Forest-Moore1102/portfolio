6e30ed2c9d06bae282db50336c74fbd55f431f50
Forest-Moore1102 <136638335+Forest-Moore1102@users.noreply.github.com>
2023-07-07 13:00:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let house= document.querySelector(".house");
+let mainPage= document.querySelector(".mainpage");
+let aboutMePage= document.querySelector(".abt-me-page");
+
+house.onclick=function(){
+   location.href = "aboutmepage.html";
+}
