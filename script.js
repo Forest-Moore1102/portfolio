75323e9af845eb26165e353ea4ff439a482d9a6b
Forest E. Moore <136638335+Forest-Moore1102@users.noreply.github.com>
2023-07-10 00:26:02 -0400

    On branch main  Changes to be committed: 	new file:   about-me.html 	deleted:    aboutmepage.html 	new file:   achievements.html 	new file:   hobbies.html 	new file:   projects.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-let house= document.querySelector(".house");
-let mainPage= document.querySelector(".mainpage");
-let aboutMePage= document.querySelector(".abt-me-page");
+let house = document.querySelector(".house");
+let mainPage = document.querySelector(".mainpage");
+let aboutMePage = document.querySelector(".abt-me-page");
 
-house.onclick=function(){
+house.onclick = function () {
    location.href = "aboutmepage.html";
+}
+let trophycaseButton = document.querySelector(".trophycase");
+trophycaseButton.onclick = function () {
+   location.href = "achievements.html";
+}
+let pinboardButton = document.querySelector(".pinboard");
+pinboardButtonButton.onclick = function () {
+   location.href = "hobbies.html";
+}
+let deskButton = document.querySelector(".desk");
+deskButton.onclick = function () {
+   location.href = "projects.html.html";
+}
+let nextButton1= document.querySelector(".nxt-button-1");
+let nextButton2= document.querySelector(".nxt-button-2");
+let nextButton3= document.querySelector(".nxt-button-3");
+let nextButton4= document.querySelector(".nxt-button-4");
+let nextButton5= document.querySelector(".nxt-button-5");
+
+let abtMe1= document.querySelector(".abt-me-1");
+let abtMe2= document.querySelector(".abt-me-2");
+let abtMe3= document.querySelector(".abt-me-3");
+let abtMe4= document.querySelector(".abt-me-4");
+let abtMe5= document.querySelector(".abt-me-5");
+
+nextButton1.onclick= function(){
+   abtMe2.style.display= "block";
+   abtMe1.style.display= "none";
+}
+nextButton2.onclick= function(){
+   abtMe3.style.display= "block";
+   abtMe2.style.display= "none";
+}
+nextButton3.onclick= function(){
+   abtMe4.style.display= "block";
+   abtMe3.style.display= "none";
+}
+nextButton4.onclick= function(){
+   abtMe4.style.display= "block";
+   abtMe3.style.display= "none";
+}
+nextButton5.onclick= function(){
+   location.href = "about-me.HTML";
 }
